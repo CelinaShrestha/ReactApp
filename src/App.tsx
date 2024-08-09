63cd39e621e0bd5e580b760db6fbfb6d88c26b33
Celina Shrestha <celinasth7@gmail.com>
2024-08-09 09:43:58 +0545

    Created login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import Login from "./Login";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
-    <div className="d-flex justify-content-evenly align-items-center">
-      <div className="row ">
-        <div className="col d-flex justify-content-center align-items-center">
-          <Login />
-        </div>
-        <div className="col">HEllo</div>
-      </div>
-    </div>
+    <Login />
   );
 }
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,19 @@
+import LoginForm from "../components/LoginForm";
+import background_image from "./assets/background-image.jpg";
+
+function Login() {
+  return (
+    <div className="container mx-auto">
+      <div className="row justify-content-around align-items-center p-5">
+        <div className="col-4 d-flex flex-column align-items-center ">
+          <LoginForm />
+        </div>
+        <div className="col-8 image">
+          <img src={background_image} alt="image" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
